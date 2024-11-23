405f5ffe26b4093d32d2942e4f1e165cb5caec36
gabriellereyes <ga.reyesa@duocuc.cl>
2024-11-22 23:22:49 -0300

    wsgi.py

--- ventasApp/wsgi.py
+++ ventasApp/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Asegúrate de que el entorno virtual esté activado
+path = '/home/GabrielleReyes98/proyecto/VentasApp'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Asegúrate de que Django esté usando tu entorno virtual
+activate_this = 'source /home/GabrielleReyes98/.virtualenvs/myenv/bin/activate'
+exec(open(activate_this).read(), {'__file__': activate_this})
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'VentasApp.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ventasApp.settings')
-
 application = get_wsgi_application()
